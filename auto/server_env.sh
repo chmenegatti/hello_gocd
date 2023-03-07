e592ff72030bf075d1329365c2c95e4ff26da8d7
Cesar Menegatti <cesar.menegatti@outlook.com>
2023-03-06 23:13:37 -0300

    Automation

--- auto/server_env.sh
+++ auto/server_env.sh
@@ -1,19 +1,20 @@
-#Check if exists an .env
-FILE=../.env
-if [[ -f "$FILE" ]]; then
-#File exists, removing
+echo "Check if exists a .env"
+FILE=./.env
+if [ -f "$FILE" ]; then
+
+echo "File exists, removing"
     rm $FILE
 fi
 
-#Creating an empty .env
+echo "Creating an empty .env"
 touch .env
 
-#Populanting with configs
+echo "Populanting with configs"
 echo DB_NAME=$DB_NAME >> .env
 echo DB_HOST=$DB_HOST >> .env
 echo DB_USER=$DB_USER >> .env
 echo DB_PORT=$DB_PORT >> .env
 echo DB_PASS=$DB_PASS >> .env
 
-#Showing content
+echo "Showing content"
 cat .env
